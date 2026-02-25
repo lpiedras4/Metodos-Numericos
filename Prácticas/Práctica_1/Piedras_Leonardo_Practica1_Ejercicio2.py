bde232edd88c6b1fbd67d00cb1f6d93c4e015645
lpiedras4 <piedras.leo@gmail.com>
2026-02-25 11:32:54 -0800

    empezamos cálculo manueal y definición de variables

--- Prácticas/Práctica_1/Piedras_Leonardo_Practica1_Ejercicio2.py
+++ Prácticas/Práctica_1/Piedras_Leonardo_Practica1_Ejercicio2.py
@@ -1,1 +1,20 @@
-#Series de Taylor
+#Ejercicio 2: Series de Taylor en tiempo real
+# Función f(t) = e^(-t) * cos(2t) 
+# Predecir f(0.5), sabiendo que estamos actualmente en t=0
+#Implementar en código la Serie de Taylor usando aproximaciones hasta 2do orden
+
+import math
+import numpy as np
+import matplotlib as pit
+
+#Datos
+t = 0.5
+t0 = 0 
+# Paso h = t - ti
+h = t - t0
+
+#Función a evaluar
+print("Taylor de f(t) = e^(-t) * cos(2t), t = 0.5, t0 = 0")
+
+#Derivadas en t0
+
