9267647ec3e802c78fe65b46c270ea4e38bb779e
lpiedras4 <piedras.leo@gmail.com>
2026-02-26 11:00:14 -0800

    se calcularon medias y desviaciones estandar

--- Prácticas/Práctica_1/Piedras_Leonardo_Practica1_Ejercicio4.py
+++ Prácticas/Práctica_1/Piedras_Leonardo_Practica1_Ejercicio4.py
@@ -2,22 +2,36 @@ import math
 import numpy as np
 import matplotlib as plt
 
-#Clasificar sensores como exacto/inexacto y preciso/impreciso y que miden una temperatura constante real de 100.o grados C. Toman 5 lecturas cada uno.
+#Clasificar sensores como exacto/inexacto y preciso/impreciso y que miden una temperatura constante real de 100.0 grados C. Toman 5 lecturas cada uno.
 #Exacto / inexacto (basado en la media vs valor real)
 #Preciso e impreciso ( basado en la desviación estándar)
+tempConst = 100.0
 sensor_A = [105.1, 104.9, 105.0, 105.2, 104.8]
 sensor_B = [100.0, 120.0, 80.0, 110.0, 90.0]
 sensor_C = [98.1, 101.5, 99.0, 102.1, 99.5]
 
-media_sensorA=np.mean(sensor_A)
-media_sensorB=np.mean(sensor_B)
-media_sensorC=np.mean(sensor_C)
-print(f"{media_sensorA}")
-print(f"{media_sensorB}")
-print(f"{media_sensorC}")
+def mediaSensor(sensor):
+  return np.mean(sensor)
 
-desv_est_sensorA = np.std(sensor_A)
-desv_est_sensorB = np.std(sensor_B)
-desv_est_sensorC = np.std(sensor_C)
+def desvEstSensor(sensor):
+  return np.std(sensor)
 
 #Guardar arrays sensores en una colección, iterar sobre ella y obtener medias y desvuiaciones estandar para imprimir en consola
+sensores = {"Sensor A":sensor_A, "Sensor B":sensor_B,"Sensor C":sensor_C}
+
+print("Medición de exactitud de los sensores con base en la media de temperatura")
+for sensor,temps in sensores.items():
+  if mediaSensor(temps)!=tempConst:
+    print(f"El {sensor} es inexacto: {mediaSensor(temps)}")
+  elif mediaSensor(temps)==tempConst:
+     print(f"El {sensor} es exacto: {mediaSensor(temps)}")
+
+print("Medición de precisión de los sensores con base en la desviación estandar de las temperaturas")
+for sensor,temps in sensores.items():
+  #Utilizamos el coeficiente de variación (desviación / media) * 100 y establecemos un porcentaje para definir nuestra precisión
+  #Utilizaremos un porcentaje de 5% para determinar si es preciso o impreciso
+  coef_var = (desvEstSensor(temps) / mediaSensor(temps)) * 100
+  if coef_var>5:
+    print(f"El {sensor} es impreciso: {desvEstSensor(temps)}")
+  else:
+     print(f"El {sensor} es preciso: {desvEstSensor(temps)}")
