8b036d3d834c7dd25227aeadf56c09b8ac582c32
lpiedras4 <piedras.leo@gmail.com>
2026-02-26 21:44:40 -0800

    Se terminaron todos los ejercicios

--- Prácticas/Práctica_1/Piedras_Leonardo_Practica1_Ejercicio3.py
+++ Prácticas/Práctica_1/Piedras_Leonardo_Practica1_Ejercicio3.py
@@ -1,21 +1,20 @@
-#Ejercicio 3: Límites de convergencias
-#En este ejercicio utilizaremos la Serie de Maclaurin para:
-#f(x) = ln(1+x)
+
 import math
 import numpy as np
 import matplotlib as plt
 
-def f(x): #Función f(x) = ln(1+x) como método en python
-  return np.log(1+x)
+#Ejercicio 3: Límites de convergencias
+#En este ejercicio utilizaremos la Serie de Maclaurin para:
+#Función f(x) = ln(1+x)
 
 def mi_log_natural(x_val,tolerancia):
   base = 0
   iteraciones = 0
   error_aprox = 100
-  while error_aprox > tolerancia:
-    x = base
+  while error_aprox > tolerancia: #Mientras el error aproximado sea mayor que la tolerancia, agregamos términos
     iteraciones+=1
-    maclaurin = ((-1)**(iteraciones+1))*(x_val**iteraciones) / iteraciones
+    x = base
+    maclaurin = ((-1)**(iteraciones+1)) * (x_val**iteraciones) / iteraciones
     base+=maclaurin
     
     #Cálculo del error aproximado
@@ -23,9 +22,16 @@ def mi_log_natural(x_val,tolerancia):
       error_aprox = abs((base-x)/base) * 100
   return base,iteraciones
 
-#Probamos en ln(0.5)
+#Evaluación en ln(1.5)
 resultado, iter = mi_log_natural(0.5,0.001)
-real = f(0.5)
+real = np.log(1.5)
 print(f"Aproximación:{resultado:.8f}")
 print(f"Valor real: {real:.8f}")
+print(f"Iteraciones: {iter}")
+
+#Evaluación  en ln(3.0)
+resultado, iter = mi_log_natural(2,0.001)
+real = np.log(3)
+print(f"Aproximación: {resultado:.8f}")
+print(f"Valor real:   {real:.8f}")
 print(f"Iteraciones: {iter}")
